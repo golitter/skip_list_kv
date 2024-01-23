4e7d1f066984ef3735dc3898a442777ef379c630
golitter_wsl2 <1981971524@qq.com>
2024-01-23 10:05:28 +0800

    Add automatic build scripts - auto_build.h

---
+++ auto_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 本在任何命令返回非零退出码时立即停止执行，并输出相应的错误信息
+set -e
+mkdir -p build
+cd build
+
+# 生成MakeFile文件
+cmake ..
+
+# 构建可执行文件
+make -j8
+
+# 执行可执行文件
+./skip_list_kv
+
+# 退回到原来目录下
+cd ..
+
